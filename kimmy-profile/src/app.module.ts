8634644d02aef292e48e42f2e9ca4faf1aa4c13e
KimyFuchs <Karim.u506203@gmail.com>
2024-05-23 10:16:06 -0700

    rawra

--- kimmy-profile/src/app.module.ts
+++ kimmy-profile/src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { SkillsResolver } from './skills/skills.resolver';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [MongooseModule.forRoot(process.env.FOX_Url)],
   controllers: [AppController],
-  providers: [AppService, SkillsResolver],
+  providers: [AppService],
 })
 export class AppModule {}
